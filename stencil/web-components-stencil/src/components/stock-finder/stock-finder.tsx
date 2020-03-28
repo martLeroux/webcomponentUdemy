6bc1bdbf918dbb62b398bbf44bc78198cb249220
Leroux Martin <tintin_151@hotmail.com>
2020-03-28 15:42:35 -0400

    Jusqu'à #120

---
+++ stencil/web-components-stencil/src/components/stock-finder/stock-finder.tsx
@@ -0,0 +1,49 @@
+import { Component, h, State, Event, EventEmitter } from "@stencil/core";
+import { AV_API_KEY } from '../../global/global';
+
+@Component({
+    tag: 'dsc-stock-finder',
+    styleUrl: './stock-finder.css',
+    shadow: true
+})
+
+export class StockFinder {
+    stockNameInput: HTMLInputElement;
+
+    @State() searchResults: {symbol: string, name: string}[] = [];
+
+    @Event({bubbles: true, composed: true}) dscSymbolSelected: EventEmitter<string>;
+
+    onFindStock(event: Event) {
+        event.preventDefault();
+        const stockName = this.stockNameInput.value;
+        fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${stockName}&apikey=${AV_API_KEY}`)
+            .then(res => res.json())
+            .then(parsedRes => {
+                console.log(parsedRes);
+                this.searchResults = parsedRes['bestMatches'].map(match => {
+                    return {name: match['2. name'], symbol: match['1. symbol']}
+                });
+            }).catch(err => {
+                console.log(err);
+            });
+    }
+
+    onSelectSymbol(symbol: string) {
+        this.dscSymbolSelected.emit(symbol);
+    }
+
+    render() {
+        return [
+            <form onSubmit={this.onFindStock.bind(this)}>
+                <input id="stock-symbol" ref={el => this.stockNameInput = el}  />
+                <button type="submit">Finde</button>
+            </form>,
+            <ul>
+                {this.searchResults.map(result => (
+                    <li onClick={this.onSelectSymbol.bind(this, result.symbol)}><strong>{result.symbol}</strong> - {result.name}</li>
+                ))}
+            </ul>
+        ]
+    }
+}
