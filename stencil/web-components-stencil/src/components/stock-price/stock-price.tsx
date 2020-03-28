1c40c6d50e7eb270cd96ca0e0ddc14242f4ddd81
Leroux Martin <tintin_151@hotmail.com>
2020-03-28 10:32:29 -0400

    jusqu'à #110

--- stencil/web-components-stencil/src/components/stock-price/stock-price.tsx
+++ stencil/web-components-stencil/src/components/stock-price/stock-price.tsx
@@ -12,6 +12,7 @@ export class StockPrice {
     @State() fetchedPrice: number;
     @State() stockUserInput: string;
     @State() stockInputValid = false;
+    @State() error = '';
 
     onUserInput(event: Event) {
         this.stockUserInput = (event.target as HTMLInputElement).value;
@@ -33,22 +34,35 @@ export class StockPrice {
             return res.json();
         })
         .then(parsedRes => {
+            if (!parsedRes['Global Quote']['05. price']) {
+                throw new Error('Invalid!!');
+            }
+            this.error = null;
             this.fetchedPrice = +parsedRes['Global Quote']['05. price'];
         })
         .catch(err => {
             console.log(err);
+            this.error = err.message;
         });
     }
 
 
     render() {
+        let dataContent = <p>Please enter a stock symbol</p>;
+        if (this.fetchedPrice) {
+            dataContent = <p>Price: ${this.fetchedPrice}</p>
+        }
+        if (this.error) {
+            dataContent = <p>{this.error}</p>
+        }
+        
         return [
             <form onSubmit={this.onFetchStockPrice.bind(this)}>
                 <input id="stock-symbol" ref={el => this.stockInput = el} value={this.stockUserInput} onInput={this.onUserInput.bind(this)} />
                 <button type="submit" disabled={!this.stockInputValid}>Fetch</button>
             </form>,
             <div>
-                <p>Price: ${this.fetchedPrice}</p>
+                {dataContent}
             </div>
         ];
     }
