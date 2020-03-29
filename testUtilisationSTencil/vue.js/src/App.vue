da42734a7458c5105f4f9561d98ed991e40901af
Leroux Martin <tintin_151@hotmail.com>
2020-03-29 10:03:49 -0400

    Ajoute un example vue.js

--- testUtilisationSTencil/vue.js/src/App.vue
+++ testUtilisationSTencil/vue.js/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <dsc-stock-finder></dsc-stock-finder>
+    <dsc-stock-price stock-symbol="AAPL"></dsc-stock-price>
   </div>
 </template>
 
--- testUtilisationSTencil/vue.js/src/main.js
+++ testUtilisationSTencil/vue.js/src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import  { defineCustomElements } from 'test-mart-web-components-stencil/loader';
+
+
 Vue.config.productionTip = false
+
+defineCustomElements(window);
 
 new Vue({
   render: h => h(App),
